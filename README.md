2c1f8b7ef59853ffe0160d272f42680dad5ddbdd
Stu Hood <stuhood@gmail.com>
2010-10-16 22:41:37 -0400

    Extend clean and compile targets and add LaTeX inputs to the watchlist to support '~'.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-LaTeX template from http://jblevins.org/projects/cv-template/
+Build environment for LaTeX files: specifically, the CV template from http://jblevins.org/projects/cv-template/
 
-To generate a PDF in Ubuntu 10.4, you'll need to install the following packages:
+To generate PDFs in Ubuntu 10.4, you'll need to install the following packages:
   texlive-fonts-recommended texlive-latex-base texlive-latex-extra texlive-latex-recommended
 along with SBT 0.7.4 from:
   http://code.google.com/p/simple-build-tool/
 
-Then run {{sbt latex}} to generate the output PDF in target/. The project doesn't support conditional/continual rebuilding of the PDF, but it shouldn't be hard to add.
+Use {{sbt latex}} to generate the output PDF in target/. You can also run {{sbt ~latex}} to have SBT watch the input LaTeX files and automatically rebuild when they change: combine with a PDF viewer that supports automatically refreshing a document for best results.
--- project/build/Resume.scala
+++ project/build/Resume.scala
@@ -4,16 +4,32 @@ import Process._
 class ResumeProject(info: ProjectInfo) extends DefaultProject(info)
 {
     def sources = "src" / "main" / "latex" * "*.tex"
-    def target = path("target")
+    def target = "target" / "latex"
 
-    def compileLaTeX = task {
-      for (source <- sources.get) {
-        log.info("Compiling %s".format(source))
-        val process = new java.lang.ProcessBuilder("pdflatex", source.absolutePath.toString)
-        // execute the process in the working directory 'target' and log the output
-        (process directory target.asFile) ! log
-      }
-      None
+    def purge(quiet: Boolean) = FileUtilities.clean(target :: Nil, quiet, log)
+
+    def compileLatexAction = fileTask(target from sources) {
+        purge(true)
+        target.asFile.mkdir
+        for (source <- sources.get) {
+            log.info("Compiling %s".format(source))
+            val process = new java.lang.ProcessBuilder("pdflatex", source.absolutePath.toString)
+            // execute the process in the working directory 'target' and log the output
+            (process directory target.asFile) !! log
+        }
+        None
     } describedAs("Generates PDF output from LaTeX inputs.")
-    lazy val latex = compileLaTeX
+    lazy val latex = compileLatexAction
+
+    def cleanLatexAction = task {
+        purge(false)
+        None
+    } describedAs("Deletes the LaTeX output in %s.".format(target))
+    lazy val cleanLatex = cleanLatexAction
+    
+    // compile latex on compile
+    override def compileAction = super.compileAction dependsOn latex
+    override def watchPaths = super.watchPaths +++ sources
+    // clean latex output on clean
+    override def cleanAction = super.cleanAction dependsOn cleanLatex
 }
